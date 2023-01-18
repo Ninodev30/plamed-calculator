6678e132efa22832783522ef9029a7490aa98ad5
ninodev30 <ninodm.dev@gmail.com>
2023-01-17 22:48:38 -0300

    feat: create the html inputs

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,22 @@
-const handleCalculate = {
+const tableData = {
+    amb: {
+
+    },
+    cbhpm: {
+        3: {
+            uco: 11.5,
+            carry: {
+                1: {
+                    a: 8,
+                    b: 16,
+                    c: 24,
+                }
+            }
+        }
+    }
+};
+
+const handleTable = {
     amb: () => {
         console.log('testing amb');
     },
