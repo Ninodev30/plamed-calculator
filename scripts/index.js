834284a169367a457025f9f12e520f9f488ff936
ninodev30 <ninodm.dev@gmail.com>
2023-01-17 22:07:25 -0300

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+const handleCalculate = {
+    amb: () => {
+        console.log('testing amb');
+    },
+    cbhpm3: () => {
+        console.log('testing cbhpm3');
+    },
+    cbhpm4: () => {
+        console.log('testing cbhpm4');
+    },
+    cbhpm5: () => {
+        console.log('testing cbhpm5');
+    },
+};
